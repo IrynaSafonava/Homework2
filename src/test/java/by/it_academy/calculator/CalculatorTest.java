ea007631d35c96a1df2eef72be4e747cff12939e
iryna_s <emily_89@mail.ru>
2022-06-20 11:13:44 +0300

    fix: result messages wording

--- src/test/java/by/it_academy/calculator/CalculatorTest.java
+++ src/test/java/by/it_academy/calculator/CalculatorTest.java
@@ -14,31 +14,36 @@ public class CalculatorTest {
     @ParameterizedTest
     @CsvSource(value = {"-5, 8, 3", "53.89, 24.11, 78", "2147483647, 1, 2147483648"})
     public void testSummarize(double x, double y, double expectedResult) {
-        assertEquals(expectedResult, testSubject.toSummarize(x,y), "Not equal to " + expectedResult);
+        assertEquals(expectedResult, testSubject.toSummarize(x, y),
+                String.format("Summing is failed for %s and %s", x, y));
     }
 
     @ParameterizedTest
     @CsvSource(value = {"-5, 8, -13", "53.89, 24.11, 29.78", "-2147483647, 1, -2.147483648E9"})
     public void testSubtract(double x, double y, double expectedResult) {
-        assertEquals(expectedResult, testSubject.toSubtract(x,y), "Not equal to " + expectedResult);
+        assertEquals(expectedResult, testSubject.toSubtract(x, y),
+                String.format("Subtraction is failed for %s and %s", x, y) );
     }
 
     @ParameterizedTest
     @CsvSource(value = {"-5, 8, -40", "53.89, 24.11, 1299.2879", "-2147483647, -2, 4.294967294E9"})
     public void testMultiply(double x, double y, double expectedResult) {
-        assertEquals(expectedResult, testSubject.toMultiply(x,y),"Not equal to " + expectedResult);
+        assertEquals(expectedResult, testSubject.toMultiply(x, y),
+                String.format("Multiplication is failed for %s and %s", x, y));
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"-5, 0, 0", "0, -5, 0", "53.89, 0, 0", "-2147483647, 0, 0"})
-    public void testMultiplyByZero(double x, double y, double expectedResult) {
-        assertEquals(expectedResult, testSubject.toMultiply(x,y),"Not equal to " + expectedResult);
+    @CsvSource(value = {"-5, 0", "0, -5", "53.89, 0", "-2147483647, 0"})
+    public void testMultiplyByZero(double x, double y) {
+        assertEquals(0, testSubject.toMultiply(x, y),
+                String.format("Multiplication by zero is failed for %s and %s", x, y));
     }
 
     @ParameterizedTest
     @CsvSource(value = {"-5, 8, -0.625", "53.89, 24.11, 2.2351721277478225", "-2147483647, 2, -1073741823.5"})
     public void testDivide(double x, double y, double expectedResult) {
-        assertEquals(expectedResult, testSubject.toDivide(x,y), "Not equal to " + expectedResult);
+        assertEquals(expectedResult, testSubject.toDivide(x, y),
+                String.format("Division is failed for %s and %s", x, y));
     }
 
     @ParameterizedTest
